a6f4fa1459880c152ed3fbf7956a744452eebd7d
torinunna <torinunna9588@gmail.com>
2022-11-25 15:25:44 +0900

    initial setting: change rootVC to HomeVC, delete VC, main.storyboard

---
+++ NetfilxPractice/HomeViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HomeViewController.swift
+//  NetfilxPractice
+//
+//  Created by YUJIN KWON on 2022/11/25.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+    
+    
+    
+}
